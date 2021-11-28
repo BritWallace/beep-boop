d6cdbfe3fba8f07db5fea03e5370a5fb972cda0a
Brit Wallace <britwallace888@gmail.com>
2021-11-28 13:15:00 -0800

    Working number array

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,90 @@
 //Business Logic
-
-  function number(input) {
-    // let number = input.split('');
-    const word3 = "Won't you be my neighbor?";
-    const word2 = "boop";
-    const word1 = "beep";
-    if (input === ("3")) {
-      return word3;
-    } else if (input === ("2")) {
-        return word2;
-    } else if  (input === ("1")) {
-        return  word1;
-    }
+function changeArray(input){
+  let counter = input
+  const myArray = ['']
+  for (let i = 0; i <= input; i++) {
+    // myArray[i] = '' + counter[i];
+    myArray.push(counter)
+    counter -= 1
   }
-  
+  return myArray
+}
+function number(input) {
+      // let number = input.split('');
+      const word3 = "Won't you be my neighbor?";
+      const word2 = "boop";
+      const word1 = "beep";
+      if (input === ("3")) {
+        return word3;
+      } else if (input === ("2")) {
+          return word2;
+      } else if  (input === ("1")) {
+          return  word1;
+      }
+    }
 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // let output = [];
+  // const array = input('');
+  // let newArray = []
+  // let number = input
+  
+  // for(let i =0; i<array.length; i++){
+  //   newArray[i] = string.split(i);
+
+  // }
+
+  // console.log(newArray)
+    // if(array[i].includes ('3')){
+    //   output.push("Won't you be my neighbor?")
+    // } else {
+      // output.push(array[i]);
+//     }
+//   }
+//   return output;
+}
+// for(let i =0; i<number(input); i++){
+//   newArray[i] = string.split(i);
+
+
+
+
+/
+
+
+    //   } else if(array[i].includes ('2')){
+    //     output.push("boob?");
+    //   } else if(array[i].includes ('1')){
+    //     output.push("beeb?");
+    //   } else {
+    //     output.push(array[i]);
+
+    // }  
+    
+// else {
+//   output.push(array[i]);
+// }
+
+
+
+
+
+/
+
+
+  // var number = []
+  //   var digits = number.toString().split('');
+  //   var realDigits = digits.map(Number)
+  //   console.log(realDigits);
   // function number (input) {
   //   let number = parseInt(input);
   //   console.log(input);
@@ -69,10 +139,7 @@
 // let digits = number.toString().split('');
 //     let realDigits = digits.map(number)
 
- //var num = 0123456789;
-  // var digits = num.toString().split('');
-  // var realDigits = digits.map(Number)
-  // console.log(realDigits);  
+
 
 
 //const output= (number).toString;
@@ -83,8 +150,7 @@
 //}/
 
 
-// var n = input
-// var digits = (""+n).split(",");
+
 
 // const words = {
 //   beep: "beep",
@@ -93,3 +159,27 @@
 // };
 
 // for (var i = 0; i < digits.length; i++) { digits[i] = +digits[i]; }
+
+
+// function pigLatin3 (text){
+//   let vowelArray = ["a", "e", "i", "o", "u"];
+//   let finalString = ""
+//   textArray = text.split(" ");
+//   textArray.forEach(function(word) {
+//     var firstVowel=word.match(/[aeiou]/).join('');
+//     var firstPosition=word.indexOf(firstVowel);
+//     if (firstPosition === 0) {
+//       finalString = finalString.concat(word+"way ");
+//     } else if (/^qu/gi.test(word)) {
+//       let newString = ""
+//       const wordSlice = word.slice(2);
+//       newString = newString.concat(wordSlice + "quay ");
+//       finalString = finalString + newString;
+//     } else {
+//     let beginning = word.slice(firstPosition);
+//     newStr =  word.substring(0, firstPosition);
+//     finalString = finalString.concat(beginning + newStr +"ay ");
+//     }
+//   });
+//   return finalString; 
+// }
